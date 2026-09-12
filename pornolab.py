13aa2c61e70e85129b812ac72d0cd711787806e9
billyy808-gif <billyy808@gmail.com>
2026-09-11 22:40:21 -1000

    Update pornolab.py

--- pornolab.py
+++ pornolab.py
@@ -6,8 +6,8 @@
 
 # Replace YOUR_USERNAME_HERE and YOUR_PASSWORD_HERE with your Pornolab username and password
 credentials = {
-    'login_username': '',
-    'login_password': '',
+    'billyy4': '',
+    'cixwac-Dorwu1-zehhys': '',
 }
 
 # Logging
